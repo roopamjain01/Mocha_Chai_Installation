e5bd5c6ac552e379481a418ef0e593cacc6aaf33
roopamjain <roopamjain@catalyst.net.nz>
2016-02-01 10:24:24 +1300

    MINOR: Added test file

---
+++ test/test.js
@@ -0,0 +1,6 @@
+var expect = require('chai').expect; //add chai
+    describe('A basic test',function(){
+       it('Should pass when every thing is ok',function(){
+        expect(true).to.be.true;
+    });
+});
